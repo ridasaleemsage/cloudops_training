64b47e074e5d9e5ab99f499ed1ef02b7c3288d16
ridasaleemsage <rida.saleem@sage.com>
2024-12-28 01:01:14 +0000

    modularized the vpc script

--- environment/staging/local.tf
+++ environment/staging/local.tf
@@ -1,20 +1,15 @@
 locals {
-  name            = "webapp"
+  name            = "webserver"
   environment     = "staging"
   region          = "eu-west-1"
   cidr            = "10.0.0.0/16"
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets = ["10.0.9.0/24", "10.0.7.0/24"]
   name_prefix     = "${local.name}.${local.environment}"
 
   base_tags = {
     Application = local.name,
     Environment = local.environment,
     Region      = local.region
-  }
-
-  # Create a map of route table IDs keyed by the "AZ" tag
-  route_table_map = {
-    for id, rt in aws_route_table.private : rt.tags.AZ => rt.id
   }
 }
