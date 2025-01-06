7bc4c52b5b788a8a75f4288927b864a336e8ff39
ridasaleemsage <rida.saleem@sage.com>
2025-01-06 12:13:19 +0000

    changed env of prod

--- environment/prod/local.tf
+++ environment/prod/local.tf
@@ -1,6 +1,6 @@
 locals {
   name            = "webserver"
-  environment     = "staging"
+  environment     = "prod"
   region          = "eu-west-1"
   cidr            = "10.0.0.0/16"
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
